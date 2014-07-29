349f03389c27bc87c35883f2be3bd151f957453d
kristenmills <kristen@kristen-mills.com>
2014-07-28 20:14:15 -0700

    Initial Commit

---
+++ scrabbli-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scrabbli-swift
+//
+//  Created by Kristen Mills on 7/28/14.
+//  Copyright (c) 2014 Kristen Mills. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
